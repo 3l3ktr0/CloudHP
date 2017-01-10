31f5ae10650f9d96db87369a29a2dfb9a98c5654
Maxime Dadoua <maxime.dadoua@gmail.com>
2017-01-10 20:30:46 +0100

    Modifs init V2

--- initV2.sh
+++ initV2.sh
@@ -54,9 +54,9 @@ cd "$(dirname "$(realpath "$0")")";
 echo "---STEP 1: DONE---"
 
 #Install python3-pip and python-openstackclient
-echo "---STEP 2: Installing python requirements---"
+echo "---STEP 2: Installing requirements (python and jq)---"
 sudo apt-get update
-sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-pip jq
 pip3 install python-openstackclient
 echo "---STEP 2: DONE---"
 
@@ -78,17 +78,17 @@ echo "---STEP 3: DONE---"
 # echo "---STEP 4: DONE---"
 
 #Install Docker-machine on Bastion VM
-echo "---STEP 3: Installing Docker-machine---"
+echo "---STEP 4: Installing Docker-machine---"
 curl -L https://github.com/docker/machine/releases/download/v0.9.0-rc2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine \
 && chmod +x /tmp/docker-machine \
 && sudo cp /tmp/docker-machine /usr/bin/docker-machine
-echo "---STEP 3: DONE---"
+echo "---STEP 4: DONE---"
 
 #Use docker-machine to create VM instances with Docker
 #Done in parallel
 #TODO:parameters
-echo "---STEP 4: Creating $NODES instances with Docker---"
-echo "---STEP 4 Estimated duration: 5 to 10 minutes---"
+echo "---STEP 5: Creating $NODES instances with Docker---"
+echo "---STEP 5 Estimated duration: 5 to 10 minutes---"
 for ((i=1; i <= $NODES; i++)); do
   uuids[$i]=$(uuidgen)
   if [[ $i -eq 1 ]]; then
@@ -96,47 +96,46 @@ for ((i=1; i <= $NODES; i++)); do
   else
     nodes[$i]=swarm-worker-${uuids[$i]}
   fi
-  sleep 10
+  sleep 20
   docker-machine create -d openstack --openstack-flavor-name="m1.small" \
   --openstack-image-name="ubuntu1404" --openstack-keypair-name="TP_Cloud_maxime"\
   --openstack-net-name="my-private-network" --openstack-sec-groups="docker-secgroup" \
   --openstack-ssh-user="ubuntu" --openstack-private-key-file="./cloud.key" --openstack-insecure \
   ${nodes[$i]} >/dev/null &
-  sleep 10
 done
 wait
-echo "---STEP 4: DONE---"
+echo "---STEP 5: DONE---"
 
 #Initialize a Swarm
-echo "---STEP 5: Initializing a Docker Swarm---"
+echo "---STEP 6: Initializing a Docker Swarm---"
 MANAGER_IP="$(docker-machine ip ${nodes[1]})"
 docker-machine ssh ${nodes[1]} "docker swarm init --advertise-addr $MANAGER_IP"
 #Retrieve swarm token
 TOKEN="$(docker-machine ssh ${nodes[1]} 'sudo docker swarm join-token -q worker')"
-echo "---STEP 5: DONE---"
+echo "---STEP 6: DONE---"
 
 #Add worker nodes to swarm
-echo "---STEP 6: Adding worker instances to Swarm---"
+echo "---STEP 7: Adding worker instances to Swarm---"
 for ((i=2; i <= $NODES; i++)); do
   #eval "$(docker-machine env swarm-${uuids[$i]})"
   cmd="sudo docker swarm join --token $TOKEN $MANAGER_IP:2377"
   docker-machine ssh ${nodes[$i]} "$cmd"
 done
-echo "---STEP 6: DONE---"
+echo "---STEP 7: DONE---"
 #eval "$(docker-machine env -u)"
 
 #Cloning repository on every instance
-echo "---STEP 7: Cloning repository on the $NODES instances---"
+echo "---STEP 8: Cloning repository on the $NODES instances---"
 for ((i=1; i <= $NODES; i++)); do
   docker-machine ssh ${nodes[$i]} "$GIT_CLONE" >/dev/null &
 done
 wait
-echo "---STEP 7: DONE---"
+echo "---STEP 8: DONE---"
 
 #Build the Docker images on every instance
 #(add other services when ready)
-echo "---STEP 8: Build Docker images on every host---"
-echo "---STEP 8 Estimated duration: about 5 minutes---"
+echo "---STEP 9: Build Docker images on every host---"
+echo "---STEP 9 Estimated duration: about 5 minutes---"
 cmd="sudo docker build ./webserver -t cloudhp_webserver && \
 sudo docker build ./db_i -t db_i && \
 sudo docker build ./db_s -t db_s && \
@@ -147,18 +146,18 @@ for ((i=1; i <= $NODES; i++)); do
   docker-machine ssh ${nodes[$i]} "cd ./cloudHP && $cmd" >/dev/null &
 done
 wait
-echo "---STEP 8: DONE---"
+echo "---STEP 9: DONE---"
 
 # #Create Swarm networking
-echo "---STEP 9: Creating the Swarm networks---"
+echo "---STEP 10: Creating the Swarm networks---"
 cmd1="sudo docker network create -d overlay swarm_services && \
 sudo docker network create -d overlay swarm_db_i && \
 sudo docker network create -d overlay swarm_db_s && \
 sudo docker network create -d overlay swarm_proxy"
-echo "---STEP 9: DONE---"
+echo "---STEP 10: DONE---"
 
 #And finally, launch the services !
-echo "---STEP 10: Starting services---"
+echo "---STEP 11: Starting services---"
 cmd2="sudo docker service create --name web -p 80:5000 --network swarm_services cloudhp_webserver && \
 sudo docker service create --name db_i --network swarm_db_i db_i && \
 sudo docker service create --name db_s --network swarm_db_s \
@@ -171,7 +170,7 @@ curl 'localhost:8080/v1/docker-flow-proxy/reconfigure?serviceName=web&servicePat
 
 #Execute commands remotely on manager
 docker-machine ssh ${nodes[1]} "$cmd1 && $cmd2"
-echo "---STEP 10: DONE---"
+echo "---STEP 11: DONE---"
 
 
 echo "---Application deployed succesfully !---"
