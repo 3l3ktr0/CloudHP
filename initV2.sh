985ae63d51b03145ba0e8b5e313c7bbec389b04a
Maxime Dadoua <maxime.dadoua@gmail.com>
2017-01-12 18:30:36 +0100

    Màj de initV2 pour persistance des données.
    Utilisation de rex-ray: testé à la main, ça marche.

--- initV2.sh
+++ initV2.sh
@@ -96,12 +96,12 @@ for ((i=1; i <= $NODES; i++)); do
   else
     nodes[$i]=swarm-worker-${uuids[$i]}
   fi
-  sleep 60
   docker-machine create -d openstack --openstack-flavor-name="m1.small" \
-  --openstack-image-name="ubuntu1604" --openstack-keypair-name="$swarmkey"\
+  --openstack-image-name="ubuntu1404" --openstack-keypair-name="$swarmkey"\
   --openstack-net-name="my-private-network" --openstack-sec-groups="docker-secgroup" \
   --openstack-ssh-user="ubuntu" --openstack-private-key-file="./$swarmkey.pem" --openstack-insecure \
   ${nodes[$i]} >/dev/null &
+  sleep 60
 done
 wait
 echo "---STEP 5: DONE---"
@@ -130,10 +130,32 @@ done
 wait
 echo "---STEP 8: DONE---"
 
+echo "---STEP 9: Installing REX-Ray on the $NODES instances---"
+cat << EOF > /tmp/config.yml
+rexray:
+  storageDrivers:
+    - openstack
+volume:
+  mount:
+    prempt: true
+openstack:
+  authUrl: $OS_AUTH_URL
+  username: $OS_USERNAME
+  password: $OS_PASSWORD
+  tenantID: $OS_TENANT_ID
+  regionName: $OS_REGION_NAME
+EOF
+for ((i=1; i <= $NODES; i++)); do
+  docker-machine ssh ${nodes[$i]} 'curl -sSL https://dl.bintray.com/emccode/rexray/install | sh -s -- stable 0.3.3'
+  docker-machine scp /tmp/config.yml ${nodes[$i]}:/tmp/config.yml
+  docker-machine ssh ${nodes[$i]} 'sudo cp /tmp/config.yml /etc/rexray/config.yml && sudo service rexray start'
+done
+echo "---STEP 9: DONE---"
+
 #Build the Docker images on every instance
 #(add other services when ready)
-echo "---STEP 9: Build Docker images on every host---"
-echo "---STEP 9 Estimated duration: about 5 minutes---"
+echo "---STEP 10: Build Docker images on every host---"
+echo "---STEP 10 Estimated duration: about 5 minutes---"
 cmd="sudo docker build ./webserver -t cloudhp_webserver && \
 sudo docker build ./db_i -t db_i && \
 sudo docker build ./db_s -t db_s && \
@@ -144,23 +166,24 @@ for ((i=1; i <= $NODES; i++)); do
   docker-machine ssh ${nodes[$i]} "cd ./cloudHP && $cmd" >/dev/null &
 done
 wait
-echo "---STEP 9: DONE---"
+echo "---STEP 10: DONE---"
 
 # #Create Swarm networking
-echo "---STEP 10: Creating the Swarm networks---"
+echo "---STEP 11: Creating the Swarm networks---"
 cmd="sudo docker network create -d overlay swarm_services && \
 sudo docker network create -d overlay swarm_db_i && \
 sudo docker network create -d overlay swarm_db_s && \
 sudo docker network create -d overlay swarm_proxy"
 docker-machine ssh ${nodes[1]} "$cmd"
-echo "---STEP 10: DONE---"
+echo "---STEP 11: DONE---"
 
 #And finally, launch the services !
-echo "---STEP 11: Starting services---"
+echo "---STEP 12: Starting services---"
 cmd="sudo docker service create --name web --network swarm_services,swarm_proxy cloudhp_webserver && \
-sudo docker service create --name db_i --network swarm_db_i db_i && \
+sudo docker service create --name db_i --network swarm_db_i \
+--mount type=volume,volume-driver=rexray,volume-opt=size=1,src=mysqldb_i,dst=/var/lib/mysql db_i && \
 sudo docker service create --name db_s --network swarm_db_s \
---constraint 'node.hostname == ${nodes[2]}' --mount type=volume,src=mysqldata,dst=/var/lib/mysql db_s && \
+--mount type=volume,volume-driver=rexray,volume-opt=size=1,src=mysqldb_s,dst=/var/lib/mysql db_s && \
 sudo docker service create --name i --network swarm_services,swarm_db_i cloudhp_i && \
 sudo docker service create --name s --network swarm_services,swarm_db_s cloudhp_s && \
 sudo docker service create --name b --network swarm_services cloudhp_b && \
@@ -170,12 +193,12 @@ curl 'localhost:8080/v1/docker-flow-proxy/reconfigure?serviceName=web&servicePat
 
 #Execute commands remotely on manager
 docker-machine ssh ${nodes[1]} "$cmd"
-echo "---STEP 11: DONE---"
+echo "---STEP 12: DONE---"
 
-echo "---STEP 12: Allocating and associating floating IP---"
+echo "---STEP 13: Allocating and associating floating IP---"
 pubip=$(openstack floating ip create -f json external-network | jq .floating_ip_address | sed 's/"//g')
 openstack server add floating ip ${nodes[1]} $pubip
-echo "---step 12: DONE---"
+echo "---step 13: DONE---"
 
 echo "---Application deployed succesfully !---"
 echo "---Launch it by going to http://$pubip/index.html?id=<ID> on your browser---"
